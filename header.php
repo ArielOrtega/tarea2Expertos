70618d6cf032040e8fea71b2aef046536c6a7965
ArielOrtega <arielortegabrenes98@gmail.com>
2021-02-05 00:27:16 -0600

    nav bar header error test

--- header.php
+++ header.php
@@ -5,12 +5,12 @@
         </div>
         <div class="collapse navbar-collapse">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item"><a class="nav-link" href="../tarea2Expertos/src/formTarea1.php">Estilo de aprendizaje</a></li>
-                <li class="nav-item"><a class="nav-link" href="../tarea2Expertos/src/formRecinto.php">Recinto de origen</a></li>
-                <li class="nav-item"><a class="nav-link" href="../tarea2Expertos/src/formSexo.php">Sexo de estudiante</a></li>
-                <li class="nav-item"><a class="nav-link" href="../tarea2Expertos/src/formEstilo.php">Aprendizaje y recinto</a></li>
-                <li class="nav-item"><a class="nav-link" href="../tarea2Expertos/src/formProfesor.php">Tipo de profesor</a></li>
-                <li class="nav-item"><a class="nav-link" href="../tarea2Expertos/src/formRedes.php">Clasificacion de redes</a></li>
+                <li class="nav-item"><a class="nav-link" href="../src/formTarea1.php">Estilo de aprendizaje</a></li>
+                <li class="nav-item"><a class="nav-link" href="../src/formRecinto.php">Recinto de origen</a></li>
+                <li class="nav-item"><a class="nav-link" href="../src/formSexo.php">Sexo de estudiante</a></li>
+                <li class="nav-item"><a class="nav-link" href="../src/formEstilo.php">Aprendizaje y recinto</a></li>
+                <li class="nav-item"><a class="nav-link" href="../src/formProfesor.php">Tipo de profesor</a></li>
+                <li class="nav-item"><a class="nav-link" href="../src/formRedes.php">Clasificacion de redes</a></li>
             </ul>
         </div>
     </nav>
